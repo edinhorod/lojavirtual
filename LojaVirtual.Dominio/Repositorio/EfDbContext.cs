c7c922ee7d1501534b90edafecad5cd5ea8d9339
edinhorod <edinho_rodrigues@hotmail.com>
2015-03-09 17:10:36 -0300

    Aula07

---
+++ LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,25 @@
+﻿using LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Dominio.Repositorio
+{
+    /// <summary>
+    /// Classe onde são referenciadas todas as classes
+    /// </summary>
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+    }
+}
