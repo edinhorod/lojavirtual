6f877e9a01c675fcc4bd35a8205195a7e7c41aef
edinhorod <edinho_rodrigues@hotmail.com>
2015-03-10 15:17:34 -0300

    Aula10

--- LojaVirtual.Web/Controllers/VitrineController.cs
+++ LojaVirtual.Web/Controllers/VitrineController.cs
@@ -1,4 +1,5 @@
 ﻿using LojaVirtual.Dominio.Repositorio;
+using LojaVirtual.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,15 +15,25 @@ namespace LojaVirtual.Web.Controllers
         public int ProdutosPorPagina = 10;
         //
         // GET: /Vitrine/
-        public ActionResult ListaProdutos(int pagina = 1)
+        public ViewResult ListaProdutos(int pagina = 1)
         {
             _repositorio = new ProdutosRepositorio();
-            var produtos = _repositorio.Produtos
+            ProdutosViewModel model = new ProdutosViewModel
+            {
+                Produtos = _repositorio.Produtos
                 .OrderBy(p => p.Descricao)
-            .Skip((pagina - 1) * ProdutosPorPagina)
-            .Take(ProdutosPorPagina);
+                .Skip((pagina - 1) * ProdutosPorPagina)
+                .Take(ProdutosPorPagina),
 
-            return View(produtos);
+                Paginacao = new Paginacao
+                {
+                    PaginaAtual = pagina,
+                    ItensPorPagina = ProdutosPorPagina,
+                    ItensTotal = _repositorio.Produtos.Count()
+                }
+            };
+
+            return View(model);
         }
     }
 }
