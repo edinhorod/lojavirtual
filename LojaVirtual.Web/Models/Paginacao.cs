ab14404cff32a1f7992ed4536226ca1c275e6a3c
edinhorod <edinho_rodrigues@hotmail.com>
2015-03-10 13:59:40 -0300

    Aula 08

---
+++ LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        /// <summary>
+        /// TOTAL DE ITENS NO BANCO
+        /// </summary>
+        public int ItensTotal { get; set; }
+        /// <summary>
+        /// TOTAL DE ITENS POR PAGINA
+        /// </summary>
+        public int ItensPorPagina { get; set; }
+        /// <summary>
+        /// QUAL A PAGINA EXIBIDA NO MOMENTO
+        /// </summary>
+        public int PaginaAtual { get; set; }
+        public int TotaPagina
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)ItensTotal / ItensPorPagina);
+            }
+        }
+    }
+}
