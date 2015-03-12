a6f81818f4e4f58cebad53a0c2474d815db44d4b
edinhorod <edinho_rodrigues@hotmail.com>
2015-03-12 08:57:58 -0300

    Aula12

--- LojaVirtual.Web/App_Start/RouteConfig.cs
+++ LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -13,6 +13,15 @@ namespace LojaVirtual.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //CRIANDO UM NOVA ROTA PARA UM URL AMIGÁVEL PARA A PAGINA DE LISTA DE PRODUTOS
+            //ANTES: ?pagina=2, DEPOIS: Pagina2
+            routes.MapRoute(
+                name:null,//NOME DA ROTA 
+                url: "Pagina{pagina}", //COMO FICARÁ A URL. {pagina} É O PARÂMETRO
+                defaults: new  {controller = "Vitrine", action = "ListaProdutos" }
+                //controller: INFORMANDO O CONTROLLER, action: QUAL PÁGINA IRÁ RECEBER ESSA ROTA
+                );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
