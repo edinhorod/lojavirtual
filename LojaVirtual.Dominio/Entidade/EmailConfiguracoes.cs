c427b95c2a9616465f34d1b8f01f1ceb84bc3d22
edinhorod <edinho_rodrigues@hotmail.com>
2015-04-06 08:53:42 -0300

    ErrorEstilo

---
+++ LojaVirtual.Dominio/Entidade/EmailConfiguracoes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LojaVirtual.Dominio.Entidade
+{
+    public class EmailConfiguracoes
+    {
+        public bool UsarSsl = false;
+
+        public string ServidorSmtp = "mail.agiledw.com.br";
+
+        public int ServidorPorta = 587;
+
+        public string Usuario = "agiledw";
+
+        public bool EscreverArquivo = false;
+
+        public string PastaArquivo = @"c:\envioemail";
+
+        public string De = "agiledw@agiledw.com.br";
+
+        public string Para = "agiledw@agiledw.com.br";
+    }
+}
--- LojaVirtual.Web/Controllers/CarrinhoController.cs
+++ LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -3,6 +3,7 @@ using LojaVirtual.Dominio.Repositorio;
 using LojaVirtual.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -66,6 +67,39 @@ namespace LojaVirtual.Web.Controllers
         public ViewResult FecharPedido()
         {
             return View(new Pedido());
+        }
+
+        [HttpPost]
+        public ViewResult FecharPedido(Pedido pedido)
+        {
+            Carrinho carrinho = ObterCarrinho();
+            EmailConfiguracoes email = new EmailConfiguracoes
+            {
+                EscreverArquivo = bool.Parse(ConfigurationManager.AppSettings["Email.EscreverArquivo"] ?? "false")
+            };
+
+            EmailPedido emailPedido = new EmailPedido(email);
+
+            if(!carrinho.ItensCarrinho.Any())
+            {
+                ModelState.AddModelError("", "Não é possível concui o pedido. Carrinho vazio!");
+            }
+
+            if(ModelState.IsValid)
+            {
+                emailPedido.ProcessarPedido(carrinho, pedido);
+                carrinho.LimparCarrinho();
+                return View("PedidoConcluido");
+            }
+            else
+            {
+                return View(pedido);
+            }
+        }
+
+        public ViewResult PedidoConcluido()
+        {
+            return View();
         }
     }
 }
