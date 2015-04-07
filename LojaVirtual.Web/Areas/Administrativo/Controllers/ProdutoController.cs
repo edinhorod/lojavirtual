8e96cc49e91ba516e4d6d2ee9dcaa963ace1060b
edinhorod <edinho_rodrigues@hotmail.com>
2015-04-07 11:10:15 -0300

    AlterarProduto01

---
+++ LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -0,0 +1,30 @@
+﻿using LojaVirtual.Dominio.Entidade;
+using LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaVirtual.Web.Areas.Administrativo.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+        //
+        // GET: /Administrativo/Produto/
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos;
+            return View(produtos);
+        }
+
+        public ViewResult Alterar(int produtoId)
+        {
+            _repositorio = new ProdutosRepositorio();
+            Produto produto = _repositorio.Produtos.FirstOrDefault(p => p.ProdutoId == produtoId);
+                return View(produto);
+        }
+	}
+}
